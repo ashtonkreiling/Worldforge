5830b6a467477dbc15edf1a38a6e0e1fb01eb116
Ashton <ashtonkreiling@gmail.com>
2025-12-16 22:00:44 -0700

    Add artifacts and heros to settlements

--- objects/settlement.py
+++ objects/settlement.py
@@ -1,6 +1,8 @@
 from objects.project import Project
 from objects.advantage import Advantage
 from objects.tag import Tag
+from objects.artifact import Artifact
+from objects.hero import Hero
 
 class Settlement:
     def __init__(
@@ -10,8 +12,9 @@ class Settlement:
             inhabitants: dict[str:int],
             projects: list[Project],
             advantages: list[Advantage],
-            tags: list[Tag]
-
+            tags: list[Tag],
+            artifacts: list[Artifact],
+            heros: list[Hero],
         ):
         self.name = name
         self.description = description
@@ -19,3 +22,5 @@ class Settlement:
         self.projects = projects
         self.advantages = advantages
         self.tags = tags
+        self.artifacts = artifacts
+        self.heros = heros
