dbffa51c75c8b030f1356159f31aadd47f995052
Ashton <ashtonkreiling@gmail.com>
2025-12-16 22:18:49 -0700

    Add factions, make settlement inherit from object

---
+++ objects/faction.py
@@ -0,0 +1,4 @@
+from objects.object import Object
+
+class Faction(Object):
+    super
--- objects/settlement.py
+++ objects/settlement.py
@@ -1,10 +1,12 @@
+from objects.object import Object
 from objects.project import Project
 from objects.advantage import Advantage
 from objects.tag import Tag
 from objects.artifact import Artifact
 from objects.hero import Hero
+from objects.faction import Faction
 
-class Settlement:
+class Settlement(Object):
     def __init__(
             self,
             name: str,
@@ -15,12 +17,12 @@ class Settlement:
             tags: list[Tag],
             artifacts: list[Artifact],
             heros: list[Hero],
+            factions: list[Faction],
         ):
-        self.name = name
-        self.description = description
         self.inhabitants = inhabitants
         self.projects = projects
         self.advantages = advantages
-        self.tags = tags
         self.artifacts = artifacts
         self.heros = heros
+        self.factions = factions
+        super.__init__(name, description, tags)
