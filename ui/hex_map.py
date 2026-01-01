7b755ca271028176eea421b39d2ee5aa00f9f8b8
Ashton <ashtonkreiling@gmail.com>
2025-12-31 22:14:35 -0700

    Add margin to negative Q radii to prevent over-culling

--- ui/hex_map.py
+++ ui/hex_map.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 
 from ui.hex import Hex
 
@@ -6,6 +7,7 @@ class HexMap:
     def __init__(self, radius, hex_size, center):
         self.hexes = []
         self.center = center
+        self.hex_size = hex_size
         self.camera_offset = [0, 0]  # x, y
 
         for q in range(-radius, radius + 1):
@@ -14,8 +16,27 @@ class HexMap:
                     self.hexes.append(Hex(q, r, hex_size, center))
 
     def draw(self, surface):
+        screen_rect = surface.get_rect()
+        offset_x, offset_y = self.camera_offset
+        hex_size = self.hex_size
+
+        # Compute approx max number of hexes that fit horizontally and vertically
+        hex_width = math.sqrt(3) * hex_size
+        hex_height = 2 * hex_size
+        horiz_radius = int(screen_rect.width / hex_width) + 2
+        vert_radius = int(screen_rect.height / (hex_height * 3/4)) + 2
+
+        # Compute camera center in axial coordinates
+        cam_q = round((screen_rect.centerx - offset_x) / (hex_size * math.sqrt(3)))
+        cam_r = round((screen_rect.centery - offset_y) / (hex_size * 3/2))
+
+        # Only draw hexes within this axial range
         for hex in self.hexes:
-            hex.draw(surface, offset=self.camera_offset)
+            if (
+                cam_q - horiz_radius - 10 <= hex.q <= cam_q + horiz_radius and
+                cam_r - vert_radius <= hex.r <= cam_r + vert_radius
+            ):
+                hex.draw(surface, offset=self.camera_offset)
 
     def handle_event(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
